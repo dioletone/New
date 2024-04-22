8b4805a048a3d12dd058e5bcbe2c1e809983f4db
dioletone <87907834+dioletone@users.noreply.github.com>
2024-04-22 15:38:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import express from "express";
 import  sqlite3  from "sqlite3";
  const app = express();
  const port = 3000;
- let countrys =[];
+ let countrys =[{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"},{country:"gg"}];
 
  const db = new sqlite3.Database("data.db");
  const result = `SELECT country FROM DatabyCountry`;
 
    const data = db.all(result,[],(err,rows)=>
-   {
+   {  
       if (err){ console.error(err.message);} else
       
       countrys = rows;
@@ -20,6 +20,12 @@ import  sqlite3  from "sqlite3";
     res.render("./pages/page.ejs",{
       countrys : countrys,
     })
+ })
+ app.get("/country",async(req,res)=>{
+   await getList();
+   res.render("./pages/country.ejs",{
+      countrys: countrys,
+   })
  })
  app.listen(port , ()=>{
     console.log(`Listening on ${port}`);
